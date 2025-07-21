dcb5e14a03927bacd423b3ee342b7e4eee67147d
joseverdev <jmu3612@gmail.com>
2025-07-21 12:00:12 -0500

    feat: enhance Blog and Post components with dynamic content and navigation; update Presentation layout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,15 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-// 2. Import loader(s)
-import { glob, file } from 'astro/loaders';
+import { glob, file } from "astro/loaders";
 
 // 3. Define your collection(s)
-const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: 'src/content/posts' }),
+const algorithms = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "src/content/algorithms" }),
   schema: z.object({
     title: z.string(),
     // date:z.date(),
     tags: z.array(z.string()),
-  }),    
+  }),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
-export const collections = { posts };
+export const collections = {algorithms};
