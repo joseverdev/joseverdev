63d6a9b8ac1525540dcb31661c9e559dbbea6db9
joseverdev <jmu3612@gmail.com>
2025-10-14 15:40:15 -0500

    feat: add expressiveCode plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 export default defineConfig({
-  integrations: [mdx()],
-  markdown: {
-    shikiConfig: {
-      theme: 'andromeeda',
-    },
-  },
+  integrations: [
+    expressiveCode({
+      themes: ['andromeeda'],
+    }),
+    mdx(),
+  ],
 });
